f0e6e089c82323902d2444ef7c913a3ec99ea75d
Vu Than <vu.than@tpssoft.com>
2017-11-25 00:04:18 +0700

    Basic view venue.

--- client/src/app/main/host/view-venue/view-venue.component.ts
+++ client/src/app/main/host/view-venue/view-venue.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Venue } from '../../../models/venue';
+import { VenueService } from '../../../services/venue.service';
 
 @Component({
   selector: 'app-view-venue',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewVenueComponent implements OnInit {
 
-  constructor() { }
+  venue: Venue = new Venue();
+
+  constructor(
+    private route: ActivatedRoute,
+    private venueService: VenueService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.venueService.getVenue(params['contractAddress']).subscribe(venue => {
+        this.venue = venue;
+      });
+    });
   }
-
 }
