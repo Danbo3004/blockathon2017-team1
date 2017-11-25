b480506d236736a12b836a530ea04f67cee823e6
Tam Nguyen <tabenguyen@gmail.com>
2017-11-25 10:56:51 +0700

    [SC] Publish SC to Rinkeby

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -3,14 +3,14 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
     rinkeby: {
       host: "www.blockathon.asia",
-      gas: 4612388,
-      from: "0x763baAe4Db488e4d185720928639a70ea06EE196",
       port: 8545,
-      network_id: "4" // Match any network id
+      gas: 3000000,
+      network_id: "4",
+      from: "0xa2a13119a74b91bc7e7c1a039d061cbb333f721f" // Password: 123456789
     },
   }
 };
