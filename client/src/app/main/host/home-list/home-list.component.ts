6c2d2dbbffc0057c2326637fee80460680cae3c0
tnphong <tnphong435@gmail.com>
2017-11-26 07:36:50 +0700

    load homes

--- client/src/app/main/host/home-list/home-list.component.ts
+++ client/src/app/main/host/home-list/home-list.component.ts
@@ -3,6 +3,7 @@ import { HomeService } from '../../../services/home.service';
 import { Home } from '../../../models/home';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConsumerService } from '../../../services/consumer.service';
+import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
   selector: 'app-home-list',
@@ -17,14 +18,15 @@ export class HomeListComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private homeService: HomeService,
-    private consumerService: ConsumerService) { }
+    private consumerService: ConsumerService,
+    private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
-    this.homeService.getHomes('').subscribe(homes => {
-      this.homes = homes;
-    });
+    // this.homeService.getHomes('').subscribe(homes => {
+    //   this.homes = homes;
+    // });
     
-    // let address = '';
-    // this.consumerService.getHostPlaces(address).subscribe(homes => this.homes = homes);
+    let address = '0x' + this.authenticationService.wallet.getAddress().toString('hex');
+    this.consumerService.getHostPlaces(address).subscribe(homes => this.homes = homes);
   }
 }
